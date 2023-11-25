b48bfd73613eabbc726b7ccd766f3d012d6da1cf
Krystof-Bum <krystof.bumbalek@saboit.de>
2023-11-25 20:22:42 +0100

    npm

---
+++ backend/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// import generateName from "sillyName";
+// var sillyName = generateName();
+
+// console.log(sillyName);
+
+// const superheroes = require("superheroes");
+import superheroes from "superheroes";
+
+const superheroesNames = superheroes.random();
+
+console.log(superheroesNames);
