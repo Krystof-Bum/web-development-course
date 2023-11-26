f6aa0a9df6089d801b66ed44bd3c00864e3488b7
Krystof-Bum <krystof.bumbalek@saboit.de>
2023-11-26 13:11:49 +0100

    Middleware

---
+++ backend/3.4 Middleware/index4.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send(`<h1>Your name is</h1> <p>${req.body.street}${req.body.pet}</p>`);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
